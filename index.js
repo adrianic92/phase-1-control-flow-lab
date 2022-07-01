d7ff2331787da785cdb7b2a5cd74410898b23e35
Adrian <adrianic92@yahoo.com>
2022-07-01 16:09:22 -0700

    commit tree 09946b1f25af55d2af74a26bfdbcec42185a2214

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(height){
+  let phrase;
+  if (height <= 400) {
+    phrase = 'This one is on me!';
+  } else if ((height > 400) && (height < 2000)) {
+    phrase = 'That will be twenty bucks.';
+  } else if ((height => 2000) && (height <= 2500)) {
+    phrase = 'I will gladly take your thirty bucks.';
+  } else if (height > 2500) {
+    phrase = 'No can do.'
+  }
+  return phrase
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let where;
+  if (city === 'NYC') {
+    where = 'Ok, sounds good.';
+  }
+  else if (city !== 'NYC') {
+    where = 'No go.';
+  }
+  return where;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let sentence;
+  switch (tip) {
+    case 'generous':
+      sentence = 'Thank you so much.';
+      break
+    case 'not as generous':
+      sentence = 'Thank you.';
+      break
+    default:
+      sentence = 'Bye.';
+      break
+  }
+  return sentence;
 }
